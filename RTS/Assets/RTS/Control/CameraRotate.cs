c96e7d4de307cc8c758a3cc55ce2064a59a14312
Devin Weidinger <Devin.Weidinger@forgefx.com>
2022-09-06 09:51:01 -0500

    Remapped Camera Rotate to Left Alt

--- RTS/Assets/RTS/Control/CameraRotate.cs
+++ RTS/Assets/RTS/Control/CameraRotate.cs
@@ -1,3 +1,4 @@
+using RTS.Core;
 using UnityEngine;
 
 namespace RTS.Control
@@ -5,9 +6,8 @@ namespace RTS.Control
     public class CameraRotate : MonoBehaviour
     {
         [SerializeField] private float _sensitivity = 200f;
-        private static bool RightClick => Input.GetMouseButton(1);
-        private static bool RightClickDown => Input.GetMouseButtonDown(1);
-        private static bool RightClickUp => Input.GetMouseButtonUp(1);
+        private static bool RotationMod => GameData.Instance.Input.Provider.CameraRotationModifier;
+        private static bool RotationModUp => GameData.Instance.Input.Provider.CameraRotationModifierUp;
         
         private void Update()
         {
@@ -17,15 +17,15 @@ namespace RTS.Control
 
         private void Rotate()
         {
-            if (RightClick == false) return;
+            if (RotationMod == false) return;
             var xRotation = Input.GetAxis("Mouse X") * _sensitivity * Time.deltaTime;
             transform.Rotate(0, xRotation, 0, Space.World);
         }
 
         private static void UpdateCursorVisibility()
         {
-            if (RightClick) Cursor.visible = false;
-            else if (RightClickUp) Cursor.visible = true;
+            if (RotationMod) Cursor.visible = false;
+            else if (RotationModUp) Cursor.visible = true;
         }
     }
 }
--- RTS/Assets/RTS/Core/GameInput.cs
+++ RTS/Assets/RTS/Core/GameInput.cs
@@ -16,15 +16,21 @@ namespace RTS.Core
     public interface IInputProvider
     {
         Camera Camera { get; }
+        
         bool PanCamera { get; }
         bool PanCameraDown { get; }
         bool PanCameraUp { get; }
+
+        bool UnitSelection { get; }
         bool UnitSelectionDown { get; }
         bool UnitSelectionUp { get; }
-        bool UnitSelection { get; }
+        
         bool UnitInteractionDown { get; }
+        
         bool AdditiveModifier { get; }
         bool SubtractiveModifier { get; }
+        bool CameraRotationModifier { get; }
+        bool CameraRotationModifierUp { get; }
     }
 
     [Serializable]
@@ -36,6 +42,7 @@ namespace RTS.Core
         [SerializeField] private MouseButton _unitInteraction = MouseButton.RightMouse;
         [SerializeField] private KeyCode _additiveModifier = KeyCode.LeftShift;
         [SerializeField] private KeyCode _subtractiveModifier = KeyCode.LeftControl;
+        [SerializeField] private KeyCode _cameraRotationModifier = KeyCode.LeftAlt;
 
         public Camera Camera => Camera.main;
         public bool PanCamera => Input.GetMouseButton((int)_panCamera);
@@ -47,5 +54,8 @@ namespace RTS.Core
         public bool UnitInteractionDown => Input.GetMouseButtonDown((int) _unitInteraction);
         public bool AdditiveModifier => Input.GetKey(_additiveModifier);
         public bool SubtractiveModifier => Input.GetKey(_subtractiveModifier);
+        public bool CameraRotationModifier => Input.GetKey(_cameraRotationModifier);
+        public bool CameraRotationModifierUp => Input.GetKeyUp(_cameraRotationModifier);
+
     }
 }
